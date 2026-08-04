a81d09ea1c5b203fe9a7e2bfb3250f118941e99b
Ashish_Kalsara <ashishkalsara@gmail.com>
2026-08-04 23:53:54 +0530

    chnage code"

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7008b1ed82e076c4"; }
+/******/ 		__webpack_require__.h = function() { return "c2386c51f92a24af"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.7cda8b1f6dd420ba.hot-update.js
+++ .next/static/webpack/webpack.7cda8b1f6dd420ba.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e7dba7230abbc346"; }
+/******/ 	__webpack_require__.h = function() { return "c2386c51f92a24af"; }
 /******/ }();
 /******/ 
 /******/ }
